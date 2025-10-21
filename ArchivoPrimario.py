ff5ea95684197cf9dbfd7cb3c573623198b18374
David <ing.damontalvo@gmail.com>
2025-10-21 09:07:47 -0600

    Se cambio a ejemplo Amazon

--- ArchivoPrimario.py
+++ ArchivoPrimario.py
@@ -1,7 +1,16 @@
-"Primer python"
-import csv
+def sockMerchant(n, ar):
+    count= 0
+    allnumber = []
+    for i in range(len(ar)):
+        number = ar[i]
+        if number not in allnumber:
+            allnumber.append(number)
+            HowMany = ar.count(number)
+            count += int(HowMany/2)
+    return count
 
-print("RAMAB")
-print("RamaB2")
-print("Hola")
-"Tercero"
+
+n=9
+ar= [10,20,20,10,10,30,50,10,20]
+result = sockMerchant(n, ar)
+print(str(result) + '\n')
